7c0402005c54889722a4eeda0174ff6cdc098eaf
@cristhiam-quinonez Especialista en Marketing Digital | Estrategias de Crecimiento y SEO <chrisquionez354@gmail.com>
2025-08-26 01:03:58 -0600

    Create validar_imagen.py

---
+++ examples/validar_imagen.py
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Ejemplo Sobrecarga</title>
+</head>
+<body>
+    <h1>Galería de imágenes</h1>
+    <img src="imagenes/grande1.png" alt="Imagen 1" width="800">
+    <img src="imagenes/grande2.png" alt="Imagen 2" width="800">
+    <img src="imagenes/grande3.png" alt="Imagen 3" width="800">
+</body>
+</html>
